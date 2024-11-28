13dc0997f3d401df3288d0d05e034025eca2d9f7
preeti <preetipatidar1227@gmail.com>
2024-11-28 15:50:54 +0530

    third case

--- src/StringCalculator/stringCalculator.ts
+++ src/StringCalculator/stringCalculator.ts
@@ -1,8 +1,13 @@
 
 export const calculator = (numbers: string): number => {
-    if(!numbers){
-        return 0
-    }
-    return 1; 
+    // if(!numbers){
+    //     return 0
+    // }
+    // return 1; 
+
+    if (!numbers) return 0;
+    const nums = numbers.split(',').map(Number);
+     return nums.reduce((sum, num) => sum + num, 0);
+
   };
   
--- src/__test__/stringCalculator.test.ts
+++ src/__test__/stringCalculator.test.ts
@@ -8,4 +8,12 @@ describe('String Calculator', () => {
     it('return the same number for a single number', () => {
         expect(calculator("1")).toBe(1);
       });
+
+      it(' return the sum for two numbers', () => {
+        expect(calculator("1,2")).toBe(3);
+      });
+      it('it should handle any number of inputs', () => {
+        expect(calculator("1,2,3,4")).toBe(10);
+      });
+      
   });
