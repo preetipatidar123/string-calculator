a7527d33f170004fbb0ec32c60dab6816fa7eca5
preeti <preetipatidar1227@gmail.com>
2024-11-28 16:15:27 +0530

    final conditions

--- src/StringCalculator/stringCalculator.ts
+++ src/StringCalculator/stringCalculator.ts
@@ -1,13 +1,20 @@
-
 export const calculator = (numbers: string): number => {
-    // if(!numbers){
-    //     return 0
-    // }
-    // return 1; 
-
     if (!numbers) return 0;
-    const nums = numbers.split(',').map(Number);
-     return nums.reduce((sum, num) => sum + num, 0);
-
+  
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n", 2);
+      delimiter = new RegExp(parts[0].slice(2));
+      numbers = parts[1];
+    }
+  
+    const nums = numbers.split(delimiter).map(Number);
+    const negatives = nums.filter(num => num < 0);
+  
+    if (negatives.length) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+  
+    return nums.reduce((sum, num) => sum + num, 0);
   };
   
--- src/__test__/stringCalculator.test.ts
+++ src/__test__/stringCalculator.test.ts
@@ -15,5 +15,19 @@ describe('String Calculator', () => {
       it('it should handle any number of inputs', () => {
         expect(calculator("1,2,3,4")).toBe(10);
       });
+      it('it will handle newlines as delimiters', () => {
+        expect(calculator("1\n2,3")).toBe(6);
+      });
+      it("it support custom delimiters", () => {
+        expect(calculator("//;\n1;2")).toBe(3);
+      });
+    
+      it("it will throw an erorr for negative numbers", () => {
+        expect(() => calculator("1,-2,3")).toThrow("negative numbers not allowed -2");
+      });
+    
+      it(" list all negative numbers in the exception message", () => {
+        expect(() => calculator("1,-2,-3")).toThrow("negative numbers not allowed -2,-3");
+      });
       
   });
